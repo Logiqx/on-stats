317a02d5ad2b7cf5ad9817da16aae87f5c117e06
Logiqx <software@mikeg.me.uk>
2020-07-30 11:42:53 +0100

    First commit of Python script to determine shift parameters

---
+++ bin/env.sh
@@ -0,0 +1,14 @@
+PROJ_DIR=$(realpath $(dirname $0)/..)
+PROJ_NAME=$(basename $PROJ_DIR)
+WORK_DIR=/home/jovyan/work/$PROJ_NAME
+
+run_py_script()
+{
+  docker run -it --rm \
+         ${PROJ_NAME}:${IMAGE_TAG:-latest} python/$1
+}
+
+# Explanation at https://www.peterbe.com/plog/set-ex
+set -ex
+
+cd $PROJ_DIR
---
+++ bin/weekly_results.sh
@@ -0,0 +1,5 @@
+# Project Env
+. $(dirname $0)/env.sh
+
+# Determine Shift Parameters
+run_py_script determine_shift_params.py
