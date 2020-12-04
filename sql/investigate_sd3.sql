e12b385a37fb08c183757fb18aa329b412e2e1d4
Logiqx <software@mikeg.me.uk>
2020-12-04 21:00:05 +0000

    Calculate SD3 using daily ONS data

--- sql/investigate_sd3.sql
+++ sql/investigate_sd3.sql
@@ -17,7 +17,8 @@ SELECT 	YEARWEEK(DATE_ADD(DATE_ADD('2020-01-01', INTERVAL seq DAY), INTERVAL 2 D
     SUM(avg_deaths) AS avg_deaths, SUM(lower_bound) AS lower_bound, SUM(upper_bound) AS upper_bound
 FROM seq_0_to_365
 JOIN cte ON MONTH(DATE_ADD('2020-01-01', INTERVAL seq DAY)) = the_month AND DAY(DATE_ADD('2020-01-01', INTERVAL seq DAY)) = the_day
-GROUP BY ons_week;
+GROUP BY ons_week
+ORDER BY ons_week;
 
 -- Weeks in 2020 using SD3 from weekly data
 SELECT calendar_week,
@@ -29,3 +30,10 @@ WHERE calendar_year BETWEEN 2015 AND 2019
 AND calendar_week BETWEEN 1 AND 52
 GROUP BY calendar_week
 ORDER BY calendar_week;
+
+-- PHE baseline
+SELECT 	YEARWEEK(DATE_ADD(the_date, INTERVAL 2 DAY), 3) AS ons_week,
+    SUM(baseline) AS baseline, SUM(lower_bound) AS lower_bound, SUM(upper_bound) AS upper_bound
+FROM phe_baseline
+GROUP BY ons_week
+ORDER BY ons_week;
